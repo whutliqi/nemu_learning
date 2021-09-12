60d68d0742bb9a8b81b06d30f304aad9688928d0
tracer-ics2020 <tracer@njuics.org>
2021-09-12 15:44:26 +0800

    >  compile 10000000    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: e757a69dec7244b9836e021146c1b3d1            Boot ID: 6f013c458073490a887d0b4bc72674e0     Virtualization: vmware   Operating System: Debian GNU/Linux 11 (bullseye)             Kernel: Linux 5.10.0-8-amd64       Architecture: x86-64  15:44:26 up  5:37,  3 users,  load average: 0.10, 0.08, 0.02

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -123,6 +123,8 @@ static bool make_token(char *e) {
   return true;
 }
 
+int eval(int p, int q);
+bool check_parentheses(int p, int q);
 
 word_t expr(char *e, bool *success) {
   if (!make_token(e)) {
@@ -137,6 +139,81 @@ word_t expr(char *e, bool *success) {
 	printf("value is %d\n",atoi(tokens[i].str));
 	}	
   }
-
+   int ans = eval(0, nr_token-1);
+	printf("the ans is %d\n", ans);	
   return 0;
 }
+
+bool check_parentheses(int p, int q){
+	if((tokens[p].type == '(') && (tokens[q].type == ')')){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+int eval(int p, int q){
+	if(p > q){
+	
+	}
+	else if(p == q){
+		if(tokens[q].type == TK_NUM){
+			return atoi(tokens[p].str);	
+		}	
+	}
+	else if(check_parentheses(p, q) == true){
+		return eval(p+1, q-1);	
+	}
+	else{
+		int priority = 0;
+		int op_type = 0;
+		int op_index = 0;
+		int parentheses_flag = 0;
+		for(int i=p; i<q; i++){
+			if(tokens[i].type == '('){
+				parentheses_flag ++;
+			}
+			if(tokens[i].type == ')'){
+				parentheses_flag --;
+			}
+			if(parentheses_flag == 0)
+			{
+				switch(tokens[i].type){
+				case TK_NUM: break;
+				case '*':
+				case '/':
+					 {
+						if(priority <= 1){
+							op_type = tokens[i].type;
+							priority = 1;
+							op_index = i;
+						}			
+						break;
+				 }
+				case '+':
+				case '-':
+					 {
+						if(priority <= 2){
+							op_type = tokens[i].type;
+							priority = 2;
+							op_index = i;
+						}			
+						break;
+				 }
+		   	  }
+			}
+		  }
+		
+		int val1 = eval(p, op_index-1);
+		int val2 = eval(op_index+1, q);
+		switch(op_type){
+			case '+': return val1 + val2;		
+			case '-': return val1 - val2;		
+			case '*': return val1 * val2;		
+			case '/': return val1 / val2;
+			default:assert(0);		  
+		}
+		}	
+    return 0;
+}
+
